07be306961caa31f989240a367d6e5f5f54a373e
Evgenii <anarkilimitz@gmail.com>
2024-12-17 15:17:02 +0500

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const btnUp = {
         // определяем величину прокрутки
         const scrollY = window.scrollY || document.documentElement.scrollTop;
         // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-        scrollY > 400 ? this.show() : this.hide();
+        scrollY > 2380 ? this.show() : this.hide();
 });
       // при нажатии на кнопку .btn-up
     document.querySelector('.btn-up').onclick = () => {
